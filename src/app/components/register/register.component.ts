0474108aa91a615c12032144d48da401162a15b1
massndongo86@gmail.com <massndongo86@gmail.com>
2022-05-08 21:57:53 +0000

    Frontend | Mise en place de la page de création de compte

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/parameters/auth.service';
+import { LocalStorageService } from 'src/app/parameters/local-storage.service';
 
 @Component({
   selector: 'app-register',
@@ -8,13 +12,82 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   fieldTextType: boolean = false;
-  changeText: boolean = false;
+  changeText: boolean = false;  
+  inputVisisble: boolean = false;
+  submitted: boolean = false;
 
-  constructor() { }
+  errorMessage!: string;
+  failed!: string;
+  
+  register = new FormGroup({
+    pseudo: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[.#?!@$%^&*-]).{8,}$')]),
+    email: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    box: new FormControl('', [Validators.required]),
+  })
 
-  ngOnInit(): void {  
+  constructor(
+    private authService: AuthService,
+    private localStorage: LocalStorageService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    console.log(this.formControls['email']);
     
     
+  }
+
+  get formControls(){
+    return this.register.controls;
+  }
+
+  inputHidden(){
+    this.inputVisisble = !this.inputVisisble 
+  }
+  
+  create(){
+    this.submitted = true;
+    // if (this.register.invalid) {
+    //   return;
+    // }
+    const val = this.register.value ;
+    console.log(val);
+    
+    console.log(this.register.controls);
+    this.authService.createAccount(val.pseudo, val.password, val.email, val.phone).subscribe(
+      response => {
+        const token = response.tokenId;
+        this.localStorage.set('tokenId', token);
+        const status = response.status;
+        console.log(status.message);
+        
+        if (status === 'FAILED') {
+          this.failed = response.message;
+        }
+        if (token != null) {
+          this.router.navigate(["validation_code"]);
+        }
+
+        // switch (status) {
+        //   case 'FAILED_USER':
+        //     this.failed_user = response.message;
+        //     break;
+        //   case 'FAILED_PSEUDO':
+        //     this.failed_pseudo = response.message;
+        //     break;
+        //   case 'FAILED_MAIL':
+        //     this.failed_mail = response.message;
+        //     break;
+        //   case 'FAILED_PHONE':
+        //     this.failed_phone = response.message;
+        //     break;
+        //   default:
+        //     this.router.navigate(["validation_code"]);
+        // }
+      }
+    )
   }
 
   toggleFieldTextType() {
