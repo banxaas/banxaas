23c865f4512ec0d0ae2099f40813b298651431c4
zlorgoncho1 <papamatardiop3@gmail.com>
2022-05-15 12:06:35 +0000

    Dev Env | Correction du Dockerfile [Backend]

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG'))
 
 ALLOWED_HOSTS = ['localhost', 'backend']
 
