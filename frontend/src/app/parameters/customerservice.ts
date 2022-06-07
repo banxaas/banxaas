84022feb380c64f1e8ce55da31282a9b17d2c993
zlorgoncho1 <papamatardiop3@gmail.com>
2022-06-07 17:23:50 +0000

    Frontend | Update des urls pour le server de test

--- frontend/src/app/parameters/customerservice.ts
+++ frontend/src/app/parameters/customerservice.ts
@@ -2,11 +2,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customer } from './customer';
+import { environment } from 'src/environments/environment'
 
 @Injectable()
 export class CustomerService {
     
-  private setUserUrl = 'http://localhost:8000/api/setUser/';
+  private setUserUrl = environment.apiUrl + 'setUser/';
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json', Authorization: 'Bearer ' +localStorage.getItem('token') })
   }
