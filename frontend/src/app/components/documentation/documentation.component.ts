6ccfd06bcaefcafe97c6a93ae2e83d2d6de912d5
massndongo86@gmail.com <massndongo86@gmail.com>
2022-10-31 13:27:57 +0000

    FRONTEND | Review - [81%]

--- frontend/src/app/components/documentation/documentation.component.ts
+++ frontend/src/app/components/documentation/documentation.component.ts
@@ -16,10 +16,20 @@ export class DocumentationComponent implements OnInit {
   blockchain!: Boolean;
   minage!: Boolean;
   pow!: Boolean;
+  bgIntro!: string;
+  bgDiff!: string;
+  bgBtc!: string;
+  bgBuy!: string;
+  bgSell!: string;
+  bgPublish!: string;
+  bgBlockchain!: string;
+  bgMinage!: string;
+  bgPow!: string;
 
   constructor() { }
 
   ngOnInit(): void {
+
 
     this.intro = true;
     this.btc = false;
@@ -131,6 +141,109 @@ export class DocumentationComponent implements OnInit {
       this.diff = false;
       this.btc = false;
       this.intro = false;
+    }
+  }
+
+
+  changeBg(value:string){
+    if (value==='intro') {
+      this.bgIntro = "background-color: #dcfce7;"
+      this.bgBtc = "background-color: white; "
+      this.bgDiff = "background-color: white; "
+      this.bgBuy = "background-color: white; "
+      this.bgSell = "background-color: white; "
+      this.bgPublish = "background-color: white; "
+      this.bgBlockchain = "background-color: white; "
+      this.bgMinage = "background-color: white; "
+      this.bgPow = "background-color: white; "
+    }
+    if (value==='btc') {
+      this.bgIntro = "background-color: white;"
+      this.bgBtc = "background-color: #dcfce7; "
+      this.bgDiff = "background-color: white; "
+      this.bgBuy = "background-color: white; "
+      this.bgSell = "background-color: white; "
+      this.bgPublish = "background-color: white; "
+      this.bgBlockchain = "background-color: white; "
+      this.bgMinage = "background-color: white; "
+      this.bgPow = "background-color: white; "
+    }
+    if (value==='diff') {
+      this.bgIntro = "background-color: white;"
+      this.bgBtc = "background-color: white; "
+      this.bgDiff = "background-color: #dcfce7; "
+      this.bgBuy = "background-color: white; "
+      this.bgSell = "background-color: white; "
+      this.bgPublish = "background-color: white; "
+      this.bgBlockchain = "background-color: white; "
+      this.bgMinage = "background-color: white; "
+      this.bgPow = "background-color: white; "
+    }
+    if (value==='buy') {
+      this.bgIntro = "background-color: white;"
+      this.bgBtc = "background-color: white; "
+      this.bgDiff = "background-color: white; "
+      this.bgBuy = "background-color: #dcfce7; "
+      this.bgSell = "background-color: white; "
+      this.bgPublish = "background-color: white; "
+      this.bgBlockchain = "background-color: white; "
+      this.bgMinage = "background-color: white; "
+      this.bgPow = "background-color: white; "
+    }
+    if (value==='sell') {
+      this.bgIntro = "background-color: white;"
+      this.bgBtc = "background-color: white; "
+      this.bgDiff = "background-color: white; "
+      this.bgBuy = "background-color: white; "
+      this.bgSell = "background-color: #dcfce7; "
+      this.bgPublish = "background-color: white; "
+      this.bgBlockchain = "background-color: white; "
+      this.bgMinage = "background-color: white; "
+      this.bgPow = "background-color: white; "
+    }
+    if (value==='publish') {
+      this.bgIntro = "background-color: white;"
+      this.bgBtc = "background-color: white; "
+      this.bgDiff = "background-color: white; "
+      this.bgBuy = "background-color: white; "
+      this.bgSell = "background-color: white; "
+      this.bgPublish = "background-color: #dcfce7; "
+      this.bgBlockchain = "background-color: white; "
+      this.bgMinage = "background-color: white; "
+      this.bgPow = "background-color: white; "
+    }
+    if (value==='blockchain') {
+      this.bgIntro = "background-color: white;"
+      this.bgBtc = "background-color: white; "
+      this.bgDiff = "background-color: white; "
+      this.bgBuy = "background-color: white; "
+      this.bgSell = "background-color: white; "
+      this.bgPublish = "background-color: white; "
+      this.bgBlockchain = "background-color: #dcfce7; "
+      this.bgMinage = "background-color: white; "
+      this.bgPow = "background-color: white; "
+    }
+    if (value==='minage') {
+      this.bgIntro = "background-color: white;"
+      this.bgBtc = "background-color: white; "
+      this.bgDiff = "background-color: white; "
+      this.bgBuy = "background-color: white; "
+      this.bgSell = "background-color: white; "
+      this.bgPublish = "background-color: white; "
+      this.bgBlockchain = "background-color: white; "
+      this.bgMinage = "background-color: #dcfce7; "
+      this.bgPow = "background-color: white; "
+    }
+    if (value==='pow') {
+      this.bgIntro = "background-color: white;"
+      this.bgBtc = "background-color: white; "
+      this.bgDiff = "background-color: white; "
+      this.bgBuy = "background-color: white; "
+      this.bgSell = "background-color: white; "
+      this.bgPublish = "background-color: white; "
+      this.bgBlockchain = "background-color: white; "
+      this.bgMinage = "background-color: white; "
+      this.bgPow = "background-color: #dcfce7; "
     }
   }
 
