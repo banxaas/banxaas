709fab69c8a82ddb0c256e38ce0d39f4c2348214
Adama Diakhaté <adamah@192.168.68.106>
2023-03-08 18:07:31 +0000

    reponsive des pages

--- frontend/src/app/components/navbar/menu/menu.component.ts
+++ frontend/src/app/components/navbar/menu/menu.component.ts
@@ -1,16 +1,17 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: '[app-menu]',
   templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
   host: {
-    'class': 'fixed md:relative z-40 h-screen md:h-auto p-4 md:p-0 overflow-y-auto bg-white md:bg-transparent w-80 md:w-auto transition-transform right-0 top-0 translate-x-full md:translate-x-0 md:justify-between md:items-center',
-    'id': 'menu-container',
-    'tabindex': '-1',
-    'aria-hidden': 'true'
-  }
+    class:
+      'md:p-0 md:bg-transparent transition-transform md:justify-between md:items-center',
+    id: 'menu-container',
+    tabindex: '-1',
+    'aria-hidden': 'true',
+  },
 })
-
 export class MenuComponent {
   constructor() {}
 }
