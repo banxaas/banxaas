432dbac90cf2fc8706d044b03e2be559f4658d49
tiokolane <princediouf95@gmail.com>
2023-07-11 09:03:58 +0000

    sentry integration

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from corsheaders.defaults import default_headers
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,6 +95,23 @@ DATABASES = {
         'PORT': os.environ.get('DATABASE_SERVER_PORT'),
     }
 }
+
+#Config Sentry
+sentry_sdk.init(
+    dsn="https://7f9020d9f8e246e7821aa61ec5639dc6@o4505500257026048.ingest.sentry.io/4505509884919808",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
